ea94e153e88abb2841bde3d58c7bd464a818b31a
Max Lehmann <max.lehmann@digital.cabinet-office.gov.uk>
2018-07-30 13:49:50 +0100

    Add temporary page
    This provides a route, controller and mostly blank page for Frontend use.
    It is intended to be temporary, until actual routes and templates
    are available.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
+  get '/dev' => 'development#index'
 
   mount GovukPublishingComponents::Engine, at: "/component-guide"
 end
