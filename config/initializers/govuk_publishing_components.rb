053c16a61eea2d5b07f0025eb80f9b8d350fb039
Max Lehmann <max.lehmann@digital.cabinet-office.gov.uk>
2018-07-30 10:27:08 +0000

    Add component guide to app

---
+++ config/initializers/govuk_publishing_components.rb
@@ -0,0 +1,7 @@
+# config/initializers/govuk_publishing_components.rb
+GovukPublishingComponents.configure do |c|
+  c.component_guide_title = "Content Data"
+  c.application_print_stylesheet = nil
+   c.application_stylesheet = "govuk_publishing_components/admin_styles"
+  c.application_javascript = nil
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
+
+  mount GovukPublishingComponents::Engine, at: "/component-guide"
 end
