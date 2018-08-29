f6e662fd4db8a31c9aaab8c1ceb0150e87331a0f
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-08-29 11:34:37 +0100

    Add displaying multiple metrics
    Allow the view to display multiple charts for multiple metrics.
    
    We add a gds-api-adapter stub to be more closely aligned to the way
    the code would be structured in the `gds-api-adapters` gem, allowing
    code that does not belong in the `MetricsService` to be abstracted into
    `GdsApi::ContentDataApi`.

--- spec/features/single_content_item_spec.rb
+++ spec/features/single_content_item_spec.rb
@@ -5,21 +5,33 @@ RSpec.describe '/metrics/base/path', type: :feature do
   end
 
   before do
-    content_data_api_has_metric('base/path',
-                           'unique_pageviews',
-                           '2000-01-01',
-                           '2050-01-01',
-                           unique_pageviews: { total: 145_000 })
-    content_data_api_has_timeseries('base/path',
-                               'unique_pageviews',
-                               '2000-01-01',
-                               '2050-01-01',
-                               unique_pageviews: [
-                                 { date: '2018-01-13', value: 101 },
-                                 { date: '2018-01-14', value: 202 },
-                                 { date: '2018-01-15', value: 303 }
-                               ])
-    visit '/metrics/unique_pageviews/base/path?from=2000-01-01&to=2050-01-01'
+    content_data_api_has_metric(base_path: 'base/path',
+                           from: '2000-01-01',
+                           to: '2050-01-01',
+                           metrics: %w[unique_pageviews page_views],
+                           payload: {
+                               unique_pageviews: { total: 145_000 },
+                               pageviews: { total: 200_000 }
+                           })
+
+    content_data_api_has_timeseries(base_path: 'base/path',
+                               from: '2000-01-01',
+                               to: '2050-01-01',
+                               metrics: %w[unique_pageviews page_views],
+                               payload: {
+                                 unique_pageviews: [
+                                   { "date" => "2018-01-13", "value" => 101 },
+                                   { "date" => "2018-01-14", "value" => 202 },
+                                   { "date" => "2018-01-15", "value" => 303 }
+                                 ],
+                                 pageviews: [
+                                     { "date" => "2018-01-13", "value" => 10 },
+                                     { "date" => "2018-01-14", "value" => 20 },
+                                     { "date" => "2018-01-15", "value" => 30 }
+                                 ]
+                               })
+
+    visit '/metrics/base/path?from=2000-01-01&to=2050-01-01&metrics[]=unique_pageviews&metrics[]=page_views'
   end
 
   it 'renders the metric for unique_pageviews' do
@@ -29,11 +41,19 @@ RSpec.describe '/metrics/base/path', type: :feature do
 
   it 'renders the metric timeseries for unique_pageviews' do
     click_on 'Unique pageviews table'
-    rows = all('table tr')
-    expect(rows.count).to eq 4
-    expect(rows[0].text).to eq 'Date'
-    expect(rows[1].text).to eq '01-13 101'
-    expect(rows[2].text).to eq '01-14 202'
-    expect(rows[3].text).to eq '01-15 303'
+    unique_pageviews_rows = find("#unique_pageviews_2018-01-13-2018-01-15_table").all('tr')
+    pageviews_rows = find("#pageviews_2018-01-13-2018-01-15_table").all('tr')
+
+    expect(unique_pageviews_rows.count).to eq 4
+    expect(unique_pageviews_rows[0].text).to eq 'Date'
+    expect(unique_pageviews_rows[1].text).to eq '01-13 101'
+    expect(unique_pageviews_rows[2].text).to eq '01-14 202'
+    expect(unique_pageviews_rows[3].text).to eq '01-15 303'
+
+    expect(pageviews_rows.count).to eq 4
+    expect(pageviews_rows[0].text).to eq 'Date'
+    expect(pageviews_rows[1].text).to eq '01-13 10'
+    expect(pageviews_rows[2].text).to eq '01-14 20'
+    expect(pageviews_rows[3].text).to eq '01-15 30'
   end
 end
