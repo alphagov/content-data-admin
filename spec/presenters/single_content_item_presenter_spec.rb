c5d47f2b7583d24178ce6926fbe4105c37688966
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-09-13 11:18:49 +0000

    Introduce SingleContentItemPresenter#metadata
    At the moment this class returns metadata in individual
    properties.
    
    This commit introduces a `metadata` getter to return the
    data in a format suitable for the metadata component.

---
+++ spec/presenters/single_content_item_presenter_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe SingleContentItemPresenter do
+  let(:from) { '2018-01-01' }
+  let(:to) { '2018-06-01' }
+  let(:metrics) do
+    {
+      'title' => 'The title',
+      'base_path' => '/the/base/path',
+      'primary_organisation_title' => 'UK Visas and Immigration',
+      'first_published_at' => '2016-09-01T00:00:00.000Z',
+      'public_updated_at' => '2017-10-01T00:00:00.000Z',
+      'document_type' => 'news_story',
+      'unique_pageviews' => 2030,
+      'pageviews' => 3000,
+      'satisfaction_score' => 33.5,
+      'number_of_internal_searches' => 120,
+    }
+  end
+
+  subject do
+    SingleContentItemPresenter.parse_metrics(metrics: metrics,
+      from: from,
+      to: to)
+  end
+
+  describe '#metadata' do
+    it 'returns a hash with the metadata' do
+      expect(subject.metadata).to eq(
+        base_path: '/the/base/path',
+        published_at: "1 September 2016",
+        last_updated: "1 October 2017",
+        publishing_organisation: 'UK Visas and Immigration',
+        document_type: "News story",
+      )
+    end
+  end
+
+  it 'returns the aggregated metrics' do
+    expect(subject).to have_attributes(
+      unique_pageviews: 2030,
+      pageviews: 3000,
+      number_of_internal_searches: 120,
+      satisfaction_score: 33.5,
+    )
+  end
+
+  it 'returns the title' do
+    expect(subject.title).to eq('The title')
+  end
+end
