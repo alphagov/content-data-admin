1bdb64df88aac839835cea67d8ee8793cb322891
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-09-19 10:49:24 +0000

    Add a unit test for ContentRowPresenter

---
+++ spec/presenters/content_row_presenter_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe ContentRowPresenter do
+  let(:row_hash) do
+    {
+      base_path: '/some/base_path',
+      document_type: 'news_story',
+      title: 'a title',
+      unique_pageviews: 200_001,
+      satisfaction_score: 0.801001,
+      satisfaction_score_responses: 301,
+    }
+  end
+
+  subject { described_class.new(row_hash) }
+
+  it 'returns the basic attributes' do
+    expect(subject).to have_attributes(
+      base_path: '/some/base_path',
+      title: 'a title',
+      unique_pageviews: 200_001
+    )
+  end
+
+  it 'converts document_type to human readable format' do
+    expect(subject.document_type).to eq('News story')
+  end
+
+  it 'formats user_satifaction_score correctly' do
+    expect(subject.user_satisfaction_score).to eq('80.1% (301 responses)')
+  end
+end
