60560933f53f0558053afa2bf9acdb29d7115d33
chao-xian <gds@kelvingan.co.uk>
2018-09-28 18:19:51 +0100

    Add formatting percentages as headline figure
    For data we want the charts to receive the satisfaction scores as
    precise percentages for example:
    
    1.232%, 99.999%
    
    but for the headline figures we want something like 23.53% to just be
    output as 24%

--- app/helpers/metrics_formatter_helper.rb
+++ app/helpers/metrics_formatter_helper.rb
@@ -4,6 +4,14 @@ module MetricsFormatterHelper
 
   def format_metric_value(metric_name, figure)
     if METRICS_MEASURED_AS_PERCENTAGES.include?(metric_name.to_s) && figure
+      number_to_percentage(figure * 100, precision: 3)
+    else
+      figure
+    end
+  end
+
+  def format_metric_headline_figure(metric_name, figure)
+    if METRICS_MEASURED_AS_PERCENTAGES.include?(metric_name.to_s) && figure
       number_to_percentage(figure, precision: 0)
     else
       figure
--- spec/helpers/metrics_formatter_helper_spec.rb
+++ spec/helpers/metrics_formatter_helper_spec.rb
@@ -1,10 +1,16 @@
 RSpec.describe MetricsFormatterHelper do
   context 'metric needs to be rendered as a percentage' do
     it 'displays value as a percentage' do
-      value = format_metric_value('satisfaction_score', 90.000)
-      expect(value).to eq '90%'
+      value = format_metric_value('satisfaction_score', 0.9000)
+      expect(value).to eq '90.000%'
+    end
+
+    it 'displays value as a percentage whole number for headline figures' do
+      value = format_metric_headline_figure('satisfaction_score', 24.13413)
+      expect(value).to eq '24%'
     end
   end
+
 
   context 'metric does not need to be rendered as a percentage' do
     it 'displays value as an integer' do
