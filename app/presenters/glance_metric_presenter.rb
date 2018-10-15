aa5666a73fa256afc150b94b969e0830d10d1611
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-15 16:05:19 +0100

    Set default increment value to 0 in Glance Metrics

--- app/presenters/glance_metric_presenter.rb
+++ app/presenters/glance_metric_presenter.rb
@@ -1,25 +1,11 @@
 class GlanceMetricPresenter
-  def initialize(metric_name, metric_value, time_period, _secondary_metric_value = nil, context: {})
+  def initialize(metric_name, _metric_value, _time_period, _secondary_metric_value = nil, _context: {})
     @metric_name = metric_name
-    @metric_value = metric_value
-    @time_period = time_period
   end
 
   def name
     short_title = I18n.t("metrics.#{@metric_name}.short_title")
     title = I18n.t("metrics.#{@metric_name}.title")
     short_title.empty? ? title : short_title
-  end
-
-  def value
-    @metric_value
-  end
-
-  def trend_percentage
-    0
-  end
-
-  def period
-    I18n.t("metrics.show.time_periods.#{@time_period}.reference")
   end
 end
