920c8974ba9c70afe35f762705a39cf2294dc8d4
Max Lehmann <max.lehmann@digital.cabinet-office.gov.uk>
2018-10-18 15:05:17 +0100

    Create frontend reference page for index

---
+++ app/assets/javascripts/core/filtertoggle.js
@@ -0,0 +1,32 @@
+function FilterToggle () {
+
+  var toggle = function(action){
+    if (action == "hide") {
+      event.preventDefault();
+      document.getElementsByClassName("filters-control--show")[0].classList.remove("govuk-visually-hidden");
+      document.getElementsByClassName("filters-control--hide")[0].classList.add("govuk-visually-hidden");
+      document.getElementsByClassName("filter-form")[0].classList.add("govuk-visually-hidden");
+    } else {
+      event.preventDefault();
+      document.getElementsByClassName("filters-control--hide")[0].classList.remove("govuk-visually-hidden");
+      document.getElementsByClassName("filters-control--show")[0].classList.add("govuk-visually-hidden");
+      document.getElementsByClassName("filter-form")[0].classList.remove("govuk-visually-hidden");
+    }
+  }
+
+  if (document.documentElement.clientWidth < 480) {
+    document.getElementsByClassName("filters-control--hide")[0].classList.remove("govuk-visually-hidden");
+    document.getElementsByClassName("filters-control__wrapper")[0].classList.remove("govuk-visually-hidden");
+  };
+
+
+  document.getElementsByClassName("filters-control__link--hide")[0].addEventListener(
+    'click', function() {toggle("hide");}, false
+  );
+  document.getElementsByClassName("filters-control__link--show")[0].addEventListener(
+    'click', function() {toggle("show");}, false
+  );
+}
+
+// Initialise
+new FilterToggle();
--- app/controllers/development_controller.rb
+++ app/controllers/development_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class DevelopmentController < ApplicationController
+  layout 'application'
+  before_action :set_constants
+
+  def set_constants
+    @fullwidth = true
+  end
 end
