633c879dc6b269863b28b8b8ae3fa256a66c72a4
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-11-21 19:05:07 +0000

    Provide data in CSV format from the ContentController

---
+++ app/presenters/content_items_csv_presenter.rb
@@ -0,0 +1,50 @@
+require 'csv'
+
+class ContentItemsCSVPresenter
+  def initialize(data_enum, document_types, organisations)
+    @data_enum = data_enum
+    @document_types = document_types
+    @organisations = organisations
+  end
+
+  def csv_rows
+    Enumerator.new do |yielder|
+      yielder << CSV.generate_line(
+        [
+          'Title',
+          'URL',
+          'Content Data Link',
+          'Document Type',
+          'Upviews',
+          'Satisfaction',
+          'Satisfaction Score Responses',
+          'Searches'
+        ]
+      )
+
+      @data_enum.each do |result_row|
+        yielder << CSV.generate_line(
+          [
+            result_row[:title],
+            result_row[:base_path],
+            content_data_link(result_row[:base_path]),
+            result_row[:document_type],
+            result_row[:upviews],
+            result_row[:satisfaction_score_responses],
+            result_row[:searches],
+          ]
+        )
+      end
+    end
+  end
+
+  def content_data_link(base_path)
+    base = Plek.new.external_url_for('content-data-admin')
+
+    base + Rails.application.routes.url_helpers.metrics_path(
+      # Remove / from the start of the base_path, as the url helper
+      # adds it in
+      base_path[1..-1]
+    )
+  end
+end
---
+++ spec/presenters/content_items_csv_presenter_spec.rb
@@ -0,0 +1,47 @@
+RSpec.describe ContentItemsCSVPresenter do
+  include GdsApi::TestHelpers::ContentDataApi
+
+  let(:document_types) { default_document_types }
+  let(:organisations) { default_organisations }
+  let(:data_enum) do
+    [
+      {
+        title: 'Title 1',
+        base_path: '/base-path-1',
+        doucment_type: 'guide',
+        upviews: 15,
+        satisfaction_score_responses: 2,
+        searches: 14
+      },
+      {
+        title: 'Title 1',
+        base_path: '/base-path-1',
+        doucment_type: 'guide',
+        upviews: 15,
+        satisfaction_score_responses: 2,
+        searches: 14
+      }
+    ]
+  end
+
+  subject { described_class.new(data_enum, document_types, organisations) }
+
+  describe '#csv_rows' do
+    it 'returns the right number of rows' do
+      expect(subject.csv_rows.to_a.length).to eq(3)
+    end
+
+    it 'correctly generates the header' do
+      header = subject.csv_rows.first
+
+      expect(header).to include('Document Type')
+    end
+
+    it 'correctly generates data rows' do
+      data_row = subject.csv_rows.to_a[1]
+
+      expect(CSV.parse_line(data_row).length).to be(7)
+      expect(data_row).to include('2')
+    end
+  end
+end
