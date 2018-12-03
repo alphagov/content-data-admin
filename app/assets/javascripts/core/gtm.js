270a1e21dfa62a4c9d05f4735f6b3fd4a5e70f02
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2018-12-03 16:50:25 +0000

    fixup! Reformat dataLayer messages with nested values

--- app/assets/javascripts/core/gtm.js
+++ app/assets/javascripts/core/gtm.js
@@ -5,23 +5,23 @@
     var form = event.target;
     var eventName = form.id;
 
-    var dateRangeTag = form.querySelector('input[name="date_range"]:checked');
-    if(dateRangeTag && dateRangeTag.value.length > 0) {
+    var dateRangeElement = form.querySelector('input[name="date_range"]:checked');
+    if(dateRangeElement && dateRangeElement.value.length > 0) {
       var message = {};
-      message[eventName] = { 'date_range': dateRangeTag.value };
+      message[eventName] = { 'date_range': dateRangeElement.value };
       window.dataLayer.push(message);
     }
 
-    var searchTermTag = form.querySelector('input[name="search_term"]');
-    if(searchTermTag && searchTermTag.value.length > 0) {
+    var searchTermElement = form.querySelector('input[name="search_term"]');
+    if(searchTermElement && searchTermElement.value.length > 0) {
       var message = {};
-      message[eventName] = { 'search_term': searchTermTag.value };
+      message[eventName] = { 'search_term': searchTermElement.value };
       window.dataLayer.push(message);
     };
 
-    var documentTypeTag = form.querySelector('select[name="document_type"]');
-    if(documentTypeTag) {
-      var documentType = documentTypeTag.selectedOptions[0].value;
+    var documentTypeElement = form.querySelector('select[name="document_type"]');
+    if(documentTypeElement) {
+      var documentType = documentTypeElement.selectedOptions[0].value;
       if(documentType.length > 0) {
         var message = {};
         message[eventName] = { 'document_type': documentType };
@@ -29,9 +29,9 @@
       }
     }
 
-    var organisationTag = form.querySelector('select[name="organisation_id"]')
-    if (organisationTag) {
-      var organisation = organisationTag.selectedOptions[0].text;
+    var organisationElement = form.querySelector('select[name="organisation_id"]')
+    if (organisationElement) {
+      var organisation = organisationElement.selectedOptions[0].text;
       if(organisation.length > 0) {
         var message = {};
         message[eventName] = { 'organisation': organisation };
