c65785d5e8d64c5a2a5b4cc39ab9ce5e5bf81c1e
Erin Raj-Staniland <erin.raj-staniland@digital.cabinet-office.gov.uk>
2018-12-03 15:41:40 +0000

    fixup! Add user to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,18 @@
 def seed
-  unless Rails.env.development? || ENV["RUN_SEEDS_IN_PRODUCTION"]
-    puts "Skipping because not in development"
-    return
-  end
-
   if User.where(name: "Test user").present?
     puts "Skipping because user already exists"
     return
   end
+  create_test_user
+end
 
+def create_test_user
   gds_organisation_id = "af07d5a5-df63-4ddc-9383-6a666845ebe9"
 
-  user = User.create!(
+  User.create!(
     name: "Test user",
+    permissions: ["signin", "gds_editor"],
     organisation_content_id: gds_organisation_id,
-    organisation_slug: "government-digital-service",
   )
 end
 
