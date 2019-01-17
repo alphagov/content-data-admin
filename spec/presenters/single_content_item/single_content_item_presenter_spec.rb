9b43ac91186d028c27ecc44c86d3f491b0d918c9
chao-xian <gds@kelvingan.co.uk>
2019-01-17 14:58:38 +0000

    Move trend_percentages presenter tests
    The single_content_item_spec is too large. Before adding more tests,
    splitting out some of the tests into their own files will help with
    reorganising and identifying any missing tests.

--- spec/presenters/single_content_item/single_content_item_presenter_spec.rb
+++ spec/presenters/single_content_item/single_content_item_presenter_spec.rb
@@ -40,6 +40,8 @@ RSpec.describe SingleContentItemPresenter do
   end
 
   it_behaves_like 'Metadata presentation'
+  it_behaves_like 'Trend percentages presentation'
+
   describe '#status' do
     context 'when content is published' do
       it 'displays nothing' do
@@ -65,81 +67,6 @@ RSpec.describe SingleContentItemPresenter do
       }
       it 'displays withdrawn and history mode' do
         expect(subject.status).to eq(I18n.t('components.metadata.statuses.withdrawn_and_historical'))
-      end
-    end
-  end
-
-  describe '#trend_percentage' do
-    it 'calculates an increase percentage change' do
-      current_period_data[:time_series_metrics] = time_series_metrics(100)
-      previous_period_data[:time_series_metrics] = time_series_metrics(50)
-
-      expect(subject.trend_percentage('upviews')).to eq(100.0)
-    end
-
-    it 'calculates an decrease percentage change' do
-      current_period_data[:time_series_metrics] = time_series_metrics(50)
-      previous_period_data[:time_series_metrics] = time_series_metrics(100)
-
-      expect(subject.trend_percentage('upviews')).to eq(-50.0)
-    end
-
-    it 'calculates an no percentage change' do
-      current_period_data[:time_series_metrics] = time_series_metrics(100)
-      previous_period_data[:time_series_metrics] = time_series_metrics(100)
-
-      expect(subject.trend_percentage('upviews')).to eq(0.0)
-    end
-
-    it 'calculates an infinite percent increase (0 to non-zero)' do
-      current_period_data[:time_series_metrics] = time_series_metrics(100)
-      previous_period_data[:time_series_metrics] = time_series_metrics(0)
-
-      expect(subject.trend_percentage('upviews')).to eq(0.0)
-    end
-
-    it 'returns `no comparison data` if there is no comparison data' do
-      current_period_data[:time_series_metrics] = time_series_metrics(100)
-      previous_period_data[:time_series_metrics] = time_series_metrics(nil, nil)
-
-      expect(subject.trend_percentage('upviews')).to eq(nil)
-    end
-
-    it 'returns `no comparison data` if there is incomplete comparison data' do
-      current_time_series = [{ date: '2018-11-25', value: 100 }, { date: '2018-11-26', value: 100 }]
-      previous_time_series = [{ date: '2018-11-24', value: 100 }]
-
-      current_period_data[:time_series_metrics] = time_series_metrics(100, current_time_series)
-      previous_period_data[:time_series_metrics] = time_series_metrics(100, previous_time_series)
-
-      expect(subject.trend_percentage('upviews')).to eq(nil)
-    end
-
-    context 'selected date range is `past-month`' do
-      it 'calculates percentage change if previous month has data for every day in it' do
-        date_range = build(:date_range, :last_month)
-        current_time_series = complete_current_data
-        previous_month_time_series = complete_previous_data
-
-        current_period_data[:time_series_metrics] = time_series_metrics(200, current_time_series)
-        previous_period_data[:time_series_metrics] = time_series_metrics(100, previous_month_time_series)
-
-        subject = SingleContentItemPresenter.new(current_period_data, previous_period_data, date_range)
-
-        expect(subject.trend_percentage('upviews')).to eq(100.0)
-      end
-
-      it 'returns nil if previous month does not have data for every day in it' do
-        build(:date_range, :last_month)
-        current_time_series = complete_current_data
-        previous_month_time_series = incomplete_previous_data
-
-        current_period_data[:time_series_metrics] = time_series_metrics(200, current_time_series)
-        previous_period_data[:time_series_metrics] = time_series_metrics(100, previous_month_time_series)
-
-        subject = SingleContentItemPresenter.new(current_period_data, previous_period_data, date_range)
-
-        expect(subject.trend_percentage('upviews')).to eq(nil)
       end
     end
   end
@@ -197,49 +124,5 @@ RSpec.describe SingleContentItemPresenter do
     it 'returns the downcased translation of the metric name' do
       expect(subject.link_text('upviews')).to eq('unique pageviews')
     end
-  end
-
-  def time_series_metrics(upviews_total = 100, upviews_timeseries = [{ date: '2018-11-25', value: 100 }])
-    [
-      {
-        name: 'upviews',
-        total: upviews_total,
-        time_series: upviews_timeseries
-      },
-      {
-        name: 'pviews',
-        total: 100,
-        time_series: [
-          {
-            date: '2018-11-25',
-            value: 100
-          }
-        ]
-      }
-    ]
-  end
-
-  def complete_previous_data
-    previous_month_time_series = []
-    (2.months.ago.to_date.beginning_of_month..2.months.ago.to_date.end_of_month).each do |date|
-      previous_month_time_series << { date: date.strftime, value: 100 }
-    end
-    previous_month_time_series
-  end
-
-  def complete_current_data
-    previous_month_time_series = []
-    (1.month.ago.to_date.beginning_of_month..1.month.ago.to_date.end_of_month).each do |date|
-      previous_month_time_series << { date: date.strftime, value: 100 }
-    end
-    previous_month_time_series
-  end
-
-  def incomplete_previous_data
-    previous_month_time_series = []
-    (2.months.ago.to_date.beginning_of_month + 10.days..2.months.ago.to_date.end_of_month).each do |date|
-      previous_month_time_series << { date: date.strftime, value: 100 }
-    end
-    previous_month_time_series
   end
 end
