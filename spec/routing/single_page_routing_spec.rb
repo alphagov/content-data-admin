6e93c0a33122a19483a6d2d4a4b9aeb6e6eeb9e5
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-01-28 15:00:52 +1000

    Simplify Single Page routes
    Removed tests for url_helpers as apart of the rails framework.
    Improved names for tests.

--- spec/routing/single_page_routing_spec.rb
+++ spec/routing/single_page_routing_spec.rb
@@ -1,35 +1,16 @@
-RSpec.describe '/metrics routing' do
-  it 'routes correctly with given base path' do
-    expect(get: '/metrics/long/base/path?date_range=past-30-days').to route_to(
+RSpec.describe 'routes for Single Data Page' do
+  it 'routes /metrics/base/path to Single Data Page for "base/path"' do
+    expect(get: '/metrics/base/path').to route_to(
       controller: 'metrics',
       action: 'show',
-      base_path: 'long/base/path',
-      date_range: 'past-30-days'
+      base_path: 'base/path'
     )
   end
 
-  it 'generates a route for the given base_path' do
-    expect(get: metrics_path('/base/path', date_range: 'past-30-days')).to route_to(
+  it 'routes /metrics to Single Data Page for the homepage' do
+    expect(get: '/metrics').to route_to(
       controller: 'metrics',
-      action: 'show',
-      base_path: 'base/path',
-      date_range: 'past-30-days'
-    )
-  end
-
-  it 'routes correctly without a base path for the homepage' do
-    expect(get: '/metrics?date_range=last-month').to route_to(
-      controller: 'metrics',
-      action: 'show',
-      date_range: 'last-month'
-    )
-  end
-
-  it 'generates a route for the homepage' do
-    expect(get: metrics_path(date_range: 'past-30-days')).to route_to(
-      controller: 'metrics',
-      action: 'show',
-      date_range: 'past-30-days'
+      action: 'show'
     )
   end
 end
