b9e25da3c4836555e99053db36a5b632bdafdf4f
chao-xian <gds@kelvingan.co.uk>
2019-01-30 12:26:06 +0000

    Add missing semi-colons

--- app/assets/javascripts/core/gtm.js
+++ app/assets/javascripts/core/gtm.js
@@ -1,61 +1,61 @@
 (function () {
   function filterFormSubmitListner (event) {
-    window.dataLayer = window.dataLayer || []
-    var form = event.target
-    var eventName = form.id
-    var message = {}
+    window.dataLayer = window.dataLayer || [];
+    var form = event.target;
+    var eventName = form.id;
+    var message = {};
 
     var dateRangeElement = form.querySelector('input[name="date_range"]:checked')
     if (dateRangeElement && dateRangeElement.value.length > 0) {
-      message = {}
+      message = {};
       message[eventName] = { 'date_range': dateRangeElement.value }
-      window.dataLayer.push(message)
+      window.dataLayer.push(message);
     }
 
     var searchTermElement = form.querySelector('input[name="search_term"]')
     if (searchTermElement && searchTermElement.value.length > 0) {
-      message = {}
+      message = {};
       message[eventName] = { 'search_term': searchTermElement.value }
-      window.dataLayer.push(message)
+      window.dataLayer.push(message);
     };
 
-    var documentTypeElement = form.querySelector('select[name="document_type"]')
+    var documentTypeElement = form.querySelector('select[name="document_type"]');
     if (documentTypeElement) {
       var documentType = documentTypeElement.selectedOptions[0].value
       if (documentType.length > 0) {
-        message = {}
-        message[eventName] = { 'document_type': documentType }
+        message = {};
+        message[eventName] = { 'document_type': documentType };
         window.dataLayer.push(message)
       }
     }
 
-    var organisationElement = form.querySelector('select[name="organisation_id"]')
+    var organisationElement = form.querySelector('select[name="organisation_id"]');
     if (organisationElement) {
-      var organisation = organisationElement.selectedOptions[0].text
+      var organisation = organisationElement.selectedOptions[0].text;
       if (organisation.length > 0) {
-        message = {}
-        message[eventName] = { 'organisation': organisation }
-        window.dataLayer.push(message)
+        message = {};
+        message[eventName] = { 'organisation': organisation };
+        window.dataLayer.push(message);
       }
     }
-  };
+  }
 
   function init () {
-    var form = document.getElementById('filter-form')
+    var form = document.getElementById('filter-form');
     if (form != null) {
-      form.addEventListener('submit', filterFormSubmitListner, false)
+      form.addEventListener('submit', filterFormSubmitListner, false);
     }
 
-    window.dataLayer = window.dataLayer || []
-    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+    window.dataLayer = window.dataLayer || [];
+    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     dataLayer.push({
       'viewport-width': viewportWidth
     })
-  };
+  }
 
   if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', init)
+    document.addEventListener('DOMContentLoaded', init);
   } else {
-    init()
+    init();
   }
-})()
+})();
