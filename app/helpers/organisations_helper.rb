8419eacc5f92d6a8cd9c2065d9fd75439e831263
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-02-01 14:52:34 +1000

    Add organisations helper
    Adds organisations helper with method to find organisation title.
    Seperated out as a helper to allow testing.

---
+++ app/helpers/organisations_helper.rb
@@ -0,0 +1,15 @@
+module OrganisationsHelper
+  ALL_ORGANISATIONS = 'all'.freeze
+  NO_ORGANISATION = 'none'.freeze
+
+  def organisation_title(organisations, id)
+    return 'No organisation' if [NO_ORGANISATION, nil].include?(id)
+    return 'All organisations' if id == ALL_ORGANISATIONS
+
+    organisation = organisations.find { |o| o[:id] == id }
+
+    return 'Unknown organisation' if organisation == nil
+
+    organisation[:name]
+  end
+end
