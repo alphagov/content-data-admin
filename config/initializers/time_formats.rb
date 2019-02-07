8355d22438ce65b082216cfecb1f7f1484d2e87d
Kelvin Gan <kelvin.gan@digital.cabinet-office.gov.uk>
2019-02-07 13:35:53 +0000

    Remove unnecessary .strip on strftime
    To get rid of padding at start of time string, instead of calling
    .strip we should use the `-` flag in the string format.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
 Date::DATE_FORMATS[:param] = '%Y-%m-%d'
 Date::DATE_FORMATS[:long_date] = '%-d %B %Y'
-Time::DATE_FORMATS[:reading_time] = ->(time) { time.strftime("%kh %-Mm").strip }
+Time::DATE_FORMATS[:reading_time] = '%-kh %-Mm'
