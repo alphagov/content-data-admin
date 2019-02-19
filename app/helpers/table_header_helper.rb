c11fe462ef197f8a709f2da9fa242fbded11414b
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-02-19 16:34:02 +0000

    Add table header helper
    This helper is to assist for formatting of the results tables ion
    content page. Functions help with the presentation of sorting and
    help icon display.

---
+++ app/helpers/table_header_helper.rb
@@ -0,0 +1,24 @@
+module TableHeaderHelper
+  HEADER_NAMES = %w(title document_type upviews satisfaction searches).freeze
+  HEADERS_WITH_HELP_ICON = %w(upviews satisfaction searches).freeze
+  REVERSE_DEFAULT_DIRECTION = %w(title document_type).freeze
+
+  def include_help_icon?(header_name)
+    HEADERS_WITH_HELP_ICON.include?(header_name)
+  end
+
+  def aria_sort(direction)
+    direction.present? ? direction + 'ending' : 'none'
+  end
+
+  def sort_param(header_name, direction)
+    if direction.present?
+      sort = Sort.new(header_name, direction)
+      sort.reverse!
+    else
+      sort = Sort.new(header_name, 'desc')
+      sort.reverse! if REVERSE_DEFAULT_DIRECTION.include?(header_name)
+    end
+    sort.to_s
+  end
+end
