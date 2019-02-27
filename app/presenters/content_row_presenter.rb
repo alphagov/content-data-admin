fe6408ec6ee9f5f91cede043dcfbc6ada1519a17
Max Lehmann <max.lehmann@digital.cabinet-office.gov.uk>
2019-02-27 10:46:58 +0000

    Change colour to de-emphasise response values.
    This splits the satisfaction data in two at the presenter level,
    adds tests for the correct responses, then presents the section in brackets
    in a paler colour than previously.

--- app/presenters/content_row_presenter.rb
+++ app/presenters/content_row_presenter.rb
@@ -2,27 +2,35 @@ class ContentRowPresenter
   include ActionView::Helpers::NumberHelper
   include ActiveSupport::Inflector
 
-  attr_reader :title, :base_path, :document_type, :upviews,
-              :user_satisfaction, :searches, :raw_document_type
+  attr_reader :title, :base_path, :document_type, :upviews, :searches,
+    :raw_document_type, :satisfaction_percentage, :satisfaction_responses
+
   def initialize(data)
     @title = data[:title]
     @base_path = format_base_path(data[:base_path])
     @raw_document_type = data[:document_type]
     @document_type = humanize(data[:document_type])
     @upviews = number_with_delimiter(data[:upviews], delimiter: ',')
-    @user_satisfaction = format_satisfaction(
-      data[:satisfaction], data[:useful_yes], data[:useful_no]
-    )
+    @satisfaction_percentage = format_satisfaction_percentage(data[:satisfaction])
+    @satisfaction_responses = format_satisfaction_responses(data[:useful_yes], data[:useful_no])
+
     @searches = number_with_delimiter(data[:searches], delimiter: ',')
   end
 
 private
 
-  def format_satisfaction(score, yes_responses, no_responses)
-    return 'No responses' unless score
+  def format_satisfaction_percentage(score)
+    "#{(score * 100).round(0)}%" if score
+  end
 
+  def format_satisfaction_responses(yes_responses, no_responses)
     total_responses = yes_responses + no_responses
-    "#{(score * 100).round(0)}% (#{number_with_delimiter(total_responses)} responses)"
+
+    if total_responses.positive?
+      "#{number_with_delimiter(total_responses)} responses"
+    else
+      "No responses"
+    end
   end
 
   def format_base_path(base_path)
