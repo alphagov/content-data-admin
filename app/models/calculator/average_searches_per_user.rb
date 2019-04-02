9d5ba200729b33473f565bd2d31ac1ded2be7918
Kelvin Gan <dev@kelvingan.co.uk>
2019-04-02 17:42:40 +0100

    Add Calculator::AverageSearchesPerUser class
    This is so that we can remove the helper class and make clearer
    the single responsibility of the class.

---
+++ app/models/calculator/average_searches_per_user.rb
@@ -0,0 +1,23 @@
+class Calculator::AverageSearchesPerUser
+  attr_reader :searches, :unique_pageviews
+
+  def self.calculate(*args)
+    new(*args).calculate
+  end
+
+  def initialize(searches:, unique_pageviews:)
+    @searches = searches
+    @unique_pageviews = unique_pageviews
+  end
+
+  def calculate
+    searches_float = searches.to_f
+    unique_pageviews_float = unique_pageviews.to_f
+
+    return 0 if searches_float.zero? || unique_pageviews_float.zero?
+
+    search_rate = (searches_float / unique_pageviews_float) * 100
+    search_rate = 100 if search_rate > 100
+    search_rate.round(2)
+  end
+end
