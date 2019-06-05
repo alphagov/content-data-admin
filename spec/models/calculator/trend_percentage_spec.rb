327fde04e5542170a49fc3175a9702a0ecdb31ac
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-06-05 17:08:18 +0100

    Fix nil in trend percentages
    If the previous value is nil, the trend percentage caluculate
    threw an error. This puts gaurds in for nil values and adds more
    unit tests.

--- spec/models/calculator/trend_percentage_spec.rb
+++ spec/models/calculator/trend_percentage_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Calculator::TrendPercentage do
   it 'returns trend percentage if previous value > 0' do
     expect(subject.calculate(2, 1)).to eq 100
   end
+
+  it 'returns trend percentage if previous value > 0' do
+    expect(subject.calculate(1, 2)).to eq(-50)
+  end
+
+  it 'returns nil if previous value is nil' do
+    expect(subject.calculate(2, nil)).to eq nil
+  end
+
+  it 'returns nil if current value is nil' do
+    expect(subject.calculate(nil, 2)).to eq nil
+  end
 end
