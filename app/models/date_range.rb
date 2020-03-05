f6d81afcfed30086e3a831fc18dde38946d1ef66
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2020-03-05 12:58:44 +0000

    Add specfic month and date option to date_range

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -1,9 +1,9 @@
 class DateRange
-  attr_reader :time_period, :to, :from
+  attr_reader :to, :from
+  attr_accessor :time_period
 
   def initialize(time_period, relative_date = nil)
     relative_date = relative_date || Time.zone.yesterday
-
     @time_period = time_period
     @to = date_to_range(relative_date, time_period)[:to]
     @from = date_to_range(relative_date, time_period)[:from]
@@ -12,12 +12,21 @@ class DateRange
   def previous
     relative_date = @from
     relative_date -= 1.day unless @time_period == "last-month"
-    DateRange.new(time_period, relative_date)
+    if specific_month_requested
+      DateRange.new(previous_specific_month)
+    else
+      DateRange.new(time_period, relative_date)
+    end
   end
 
 private
 
+  YEARS = (2018..Time.zone.today.year).to_a.freeze
+  MONTHS = Date::MONTHNAMES.compact
+
   def date_to_range(relative_date, time_period)
+    return specified_month if specific_month_requested
+
     case time_period
     when "last-month"
       {
@@ -45,10 +54,35 @@ private
         to: relative_date,
       }
     else
+      @time_period = "past-30-days"
       {
         from: relative_date - 29.days,
         to: relative_date,
       }
     end
+  end
+
+  def specific_month_requested
+    @specific_month_requested ||= months_and_years.include?(@time_period)
+  end
+
+  def previous_specific_month
+    months_and_years[months_and_years.index(time_period) - 1]
+  end
+
+  def months_and_years
+    @months_and_years ||= YEARS.flat_map do |year|
+      MONTHS.map do |month|
+        "#{month.downcase}-#{year}"
+      end
+    end
+  end
+
+  def specified_month
+    month = Date.parse(time_period)
+    {
+      from: month.beginning_of_month,
+      to: month.end_of_month,
+    }
   end
 end
