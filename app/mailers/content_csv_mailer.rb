70394f267c8505cda1b1ba6178884152e842760a
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-03-10 16:10:48 +0000

    Use mail-notify gem to deliver via GOV.UK Notify

--- app/mailers/content_csv_mailer.rb
+++ app/mailers/content_csv_mailer.rb
@@ -1,6 +1,8 @@
-class ContentCsvMailer < ApplicationMailer
+class ContentCsvMailer < Mail::Notify::Mailer
   def content_csv_email(recipient, csv_link)
     @file_url = csv_link
-    mail(to: recipient, subject: "Your GOV.UK Content Data export")
+    view_mail(ENV.fetch("GOVUK_NOTIFY_TEMPLATE_ID", "fake-test-template-id"),
+              to: recipient,
+              subject: "Your GOV.UK Content Data export")
   end
 end
