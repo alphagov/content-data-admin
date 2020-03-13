61c30cbc711625fbfbf8da826d6e497f267fbbdb
Issy Long <me@issyl0.co.uk>
2020-03-13 13:59:07 +0000

    Switch ApplicationMailer to inherit from Mail::Notify::Mailer
    - Also split `template_id` into its own method to reduce the amount of
      thinking required to read the `view_mail(` ContentCsvMailer line.
    - This is similar refactoring to
      https://github.com/alphagov/short-url-manager/commit/106c5ff4a3d920d8b1b53996fd8a0fe78cbad8ee
      in case we add more emails to this app in the future.
    - I noticed that the default from email address was `no-reply-content-data`.
      We should use the `+content-data` form for consistency with the other
      apps' default from addresses.

--- app/mailers/content_csv_mailer.rb
+++ app/mailers/content_csv_mailer.rb
@@ -1,8 +1,10 @@
-class ContentCsvMailer < Mail::Notify::Mailer
+class ContentCsvMailer < ApplicationMailer
   def content_csv_email(recipient, csv_link)
     @file_url = csv_link
-    view_mail(ENV.fetch("GOVUK_NOTIFY_TEMPLATE_ID", "fake-test-template-id"),
-              to: recipient,
-              subject: "Your GOV.UK Content Data export")
+    view_mail(template_id, to: recipient, subject: "Your GOV.UK Content Data export")
+  end
+
+  def template_id
+    @template_id ||= ENV.fetch("GOVUK_NOTIFY_TEMPLATE_ID", "fake-test-template-id")
   end
 end
