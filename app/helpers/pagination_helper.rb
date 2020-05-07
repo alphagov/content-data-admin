49e58f720d21ec40a1dd9ba7f61c45d4c21fc496
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-05-07 12:10:25 +0100

    Remove redundant RuboCop excludes
    This only lead to one issue, which can be easily fixed without disabling
    the Cop.

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -2,24 +2,24 @@ module PaginationHelper
   include Kaminari::Helpers::HelperMethods
   attr_accessor :content
 
-  def navigation_links
+  def navigation_links(presenter)
     result = {}
-    if @presenter.prev_link?
+    if presenter.prev_link?
       result = result.merge(
         previous_page: {
-          url: path_to_prev_page(@presenter.content_items),
+          url: path_to_prev_page(presenter.content_items),
           title: "Previous",
-          label: @presenter.prev_label,
+          label: presenter.prev_label,
         },
       )
     end
 
-    if @presenter.next_link?
+    if presenter.next_link?
       result = result.merge(
         next_page: {
-          url: path_to_next_page(@presenter.content_items),
+          url: path_to_next_page(presenter.content_items),
           title: "Next",
-          label: @presenter.next_label,
+          label: presenter.next_label,
         },
       )
     end
--- spec/helpers/pagination_helper_spec.rb
+++ spec/helpers/pagination_helper_spec.rb
@@ -13,15 +13,14 @@ RSpec.describe PaginationHelper do
     controller.params[:action] = "index"
     controller.params[:controller] = "content"
     controller.params[:organisation_id] = "org-1"
-    @presenter = presenter
   end
 
   context "when on the first page" do
     let(:presenter) { ContentItemsPresenter.new(search_results, search_parameters) }
     it "only returns the `Next` link" do
-      expect(navigation_links).to eq(next_page: { url: "/content?organisation_id=org-1&page=2",
-                                                  title: "Next",
-                                                  label: "2 of 3" })
+      expect(navigation_links(presenter)).to eq(next_page: { url: "/content?organisation_id=org-1&page=2",
+                                                             title: "Next",
+                                                             label: "2 of 3" })
     end
   end
 
@@ -29,9 +28,9 @@ RSpec.describe PaginationHelper do
     let(:presenter) { ContentItemsPresenter.new(search_results.merge(page: 3), search_parameters) }
 
     it "only returns the `Previous` link" do
-      expect(navigation_links).to eq(previous_page: { url: "/content?organisation_id=org-1&page=2",
-                                                      title: "Previous",
-                                                      label: "2 of 3" })
+      expect(navigation_links(presenter)).to eq(previous_page: { url: "/content?organisation_id=org-1&page=2",
+                                                                 title: "Previous",
+                                                                 label: "2 of 3" })
     end
   end
 
@@ -39,12 +38,12 @@ RSpec.describe PaginationHelper do
     let(:presenter) { ContentItemsPresenter.new(search_results.merge(page: 2), search_parameters) }
 
     it "returns `Previous` and `Next` links" do
-      expect(navigation_links).to eq(previous_page: { url: "/content?organisation_id=org-1",
-                                                      title: "Previous",
-                                                      label: "1 of 3" },
-                                     next_page: { url: "/content?organisation_id=org-1&page=3",
-                                                  title: "Next",
-                                                  label: "3 of 3" })
+      expect(navigation_links(presenter)).to eq(previous_page: { url: "/content?organisation_id=org-1",
+                                                                 title: "Previous",
+                                                                 label: "1 of 3" },
+                                                next_page: { url: "/content?organisation_id=org-1&page=3",
+                                                             title: "Next",
+                                                             label: "3 of 3" })
     end
   end
 end
