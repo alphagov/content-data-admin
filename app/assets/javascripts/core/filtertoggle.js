09d4e67595333e6bc6dbd48d7ed4d6e25d13e987
Kevin Dew <kevindew@me.com>
2021-01-06 21:44:56 +0000

    Auto fix JS linting issues
    npx standardx --fix 'app/assets/javascripts/**/*.js'

--- app/assets/javascripts/core/filtertoggle.js
+++ app/assets/javascripts/core/filtertoggle.js
@@ -1,31 +1,29 @@
 function FilterToggle () {
-
-  var toggle = function(action){
-    if (action == "hide") {
-      event.preventDefault();
-      document.getElementsByClassName("filters-control--show")[0].classList.remove("filter--hidden");
-      document.getElementsByClassName("filters-control--hide")[0].classList.add("filter--hidden");
-      document.getElementsByClassName("filter-form")[0].classList.add("filter--hidden");
+  var toggle = function (action) {
+    if (action == 'hide') {
+      event.preventDefault()
+      document.getElementsByClassName('filters-control--show')[0].classList.remove('filter--hidden')
+      document.getElementsByClassName('filters-control--hide')[0].classList.add('filter--hidden')
+      document.getElementsByClassName('filter-form')[0].classList.add('filter--hidden')
     } else {
-      event.preventDefault();
-      document.getElementsByClassName("filters-control--hide")[0].classList.remove("filter--hidden");
-      document.getElementsByClassName("filters-control--show")[0].classList.add("filter--hidden");
-      document.getElementsByClassName("filter-form")[0].classList.remove("filter--hidden");
+      event.preventDefault()
+      document.getElementsByClassName('filters-control--hide')[0].classList.remove('filter--hidden')
+      document.getElementsByClassName('filters-control--show')[0].classList.add('filter--hidden')
+      document.getElementsByClassName('filter-form')[0].classList.remove('filter--hidden')
     }
   }
 
   if (document.documentElement.clientWidth < 480) {
-    document.getElementsByClassName("filters-control--hide")[0].classList.remove("filter--hidden");
-    document.getElementsByClassName("filters-control__wrapper")[0].classList.remove("filter--hidden");
+    document.getElementsByClassName('filters-control--hide')[0].classList.remove('filter--hidden')
+    document.getElementsByClassName('filters-control__wrapper')[0].classList.remove('filter--hidden')
   };
 
-
-  document.getElementsByClassName("filters-control__link--hide")[0].addEventListener(
-    'click', function() {toggle("hide");}, false
-  );
-  document.getElementsByClassName("filters-control__link--show")[0].addEventListener(
-    'click', function() {toggle("show");}, false
-  );
+  document.getElementsByClassName('filters-control__link--hide')[0].addEventListener(
+    'click', function () { toggle('hide') }, false
+  )
+  document.getElementsByClassName('filters-control__link--show')[0].addEventListener(
+    'click', function () { toggle('show') }, false
+  )
 }
 
 // Initialise
