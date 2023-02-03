ca6dcbe847cc877038137b77ce188793227912ff
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2023-02-03 14:06:38 +0000

    Fix auto-correctable Rubocop lint errors.
    Generated with `bundle exec rubocop -a`.

--- spec/components/accessible_autocomplete_spec.rb
+++ spec/components/accessible_autocomplete_spec.rb
@@ -1,32 +1,32 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe "AccessibleAutocomplete", type: :view do
   def component_name
     "accessible_autocomplete"
   end
 
-  it 'renders select element' do
+  it "renders select element" do
     render_component(
-      id: 'basic-autocomplete',
+      id: "basic-autocomplete",
       label: { text: "Countries" },
-      options: [['United Kingdom', 'gb'], ['United States', 'us']]
+      options: [["United Kingdom", "gb"], ["United States", "us"]],
     )
 
     assert_select ".govuk-label", text: "Countries", for: "basic-autocomplete"
     assert_select "select#basic-autocomplete"
     assert_select "select[multiple]", false
     assert_select "select#basic-autocomplete option[value=gb]"
-    assert_select "select#basic-autocomplete option[value=gb]", text: 'United Kingdom'
+    assert_select "select#basic-autocomplete option[value=gb]", text: "United Kingdom"
     assert_select "select#basic-autocomplete option[value=us]"
-    assert_select "select#basic-autocomplete option[value=us]", text: 'United States'
+    assert_select "select#basic-autocomplete option[value=us]", text: "United States"
   end
 
-  it 'renders select element with selected value' do
+  it "renders select element with selected value" do
     render_component(
-      id: 'basic-autocomplete',
+      id: "basic-autocomplete",
       label: { text: "Countries" },
-      options: [['United Kingdom', 'gb'], ['United States', 'us']],
-      selected_option: ['United States', 'us']
+      options: [["United Kingdom", "gb"], ["United States", "us"]],
+      selected_option: ["United States", "us"],
     )
 
     assert_select ".govuk-label", text: "Countries", for: "basic-autocomplete"
@@ -35,29 +35,29 @@ describe "AccessibleAutocomplete", type: :view do
     assert_select "select#basic-autocomplete option[value=us][selected]"
   end
 
-  it 'does not render when no data is specified' do
+  it "does not render when no data is specified" do
     assert_empty render_component({})
   end
 
-  it 'does not render when no label is specified' do
+  it "does not render when no label is specified" do
     assert_empty render_component(
-      id: 'basic-autocomplete',
-      options: [['United Kingdom', 'gb'], ['United States', 'us']]
+      id: "basic-autocomplete",
+      options: [["United Kingdom", "gb"], ["United States", "us"]],
     )
   end
 
-  it 'does not render when no options are specified' do
+  it "does not render when no options are specified" do
     assert_empty render_component(
-      id: 'basic-autocomplete',
-      label: { text: "Countries" }
+      id: "basic-autocomplete",
+      label: { text: "Countries" },
     )
   end
 
-  it 'renders the multiple selection version correctly' do
+  it "renders the multiple selection version correctly" do
     render_component(
       multiple: true,
       label: { text: "Countries" },
-      options: [['United Kingdom', 'gb'], ['United States', 'us']]
+      options: [["United Kingdom", "gb"], ["United States", "us"]],
     )
 
     assert_select "select[multiple]"
