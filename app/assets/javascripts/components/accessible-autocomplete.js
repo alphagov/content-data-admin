8e77479eced93aa80a651fc4fb4dd86c8c27960c
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2023-04-20 14:04:14 +0100

    Remove GA tracking from accessible-autocomplete
    This code was never actually used in this app:
    
    The `track` function is only called if `data-track-category` and `data-track-action` are both set on the <select> element. These attributes would be set in `app/views/components/_accessible_autocomplete.html.erb` if a `data_attributes` local is passed in. I've had a look through all the views where we include this partial, and none of them pass any locals when rendering the partial.
    
    Given this, I'm going to remove the tracking code, which also allows us to remove the entire `onConfirm` function (as the remainder of the function is a workaround for a bug caused by the fact that we override `onConfirm`).

--- app/assets/javascripts/components/accessible-autocomplete.js
+++ app/assets/javascripts/components/accessible-autocomplete.js
@@ -13,54 +13,15 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   }
 
   AccessibleAutocomplete.prototype.init = function () {
-    this.$selectElem = this.$module.querySelector('select')
-
     var configOptions = {
-      selectElement: this.$selectElem,
+      selectElement: this.$module.querySelector('select'),
       showAllValues: true,
       confirmOnBlur: true,
       preserveNullOptions: true, // https://github.com/alphagov/accessible-autocomplete#null-options
       defaultValue: ''
     }
 
-    configOptions.onConfirm = this.onConfirm.bind(this)
-
     new accessibleAutocomplete.enhanceSelectElement(configOptions) // eslint-disable-line no-new, new-cap
-  }
-
-  AccessibleAutocomplete.prototype.onConfirm = function (label, value, removeDropDown) {
-    function escapeHTML (str) {
-      return new window.Option(str).innerHTML
-    }
-
-    if (this.$selectElem.dataset.trackCategory !== undefined && this.$selectElem.dataset.trackAction !== undefined) {
-      track(this.$selectElem.dataset.trackCategory, this.$selectElem.dataset.trackAction, label, this.$selectElem.dataset.trackOptions)
-    }
-    // This is to compensate for the fact that the accessible-autocomplete library will not
-    // update the hidden select if the onConfirm function is supplied
-    // https://github.com/alphagov/accessible-autocomplete/issues/322
-    if (typeof label !== 'undefined') {
-      if (typeof value === 'undefined') {
-        value = Array.from(this.$selectElem.querySelectorAll('option'))
-          .find(function (option) { return option.innerHTML === escapeHTML(label) })
-          .value
-      }
-
-      // if removeDropDown we are clearing the selection from outside the component
-      if (typeof value !== 'undefined' && typeof removeDropDown === 'undefined') {
-        this.$selectElem.value = value
-        this.$selectElem.dispatchEvent(new Event('change'))
-      }
-    }
-  }
-
-  function track (category, action, label, options) {
-    if (window.GOVUK.analytics && window.GOVUK.analytics.trackEvent) {
-      options = options || {}
-      options.label = label
-
-      window.GOVUK.analytics.trackEvent(category, action, options)
-    }
   }
 
   Modules.AccessibleAutocomplete = AccessibleAutocomplete
