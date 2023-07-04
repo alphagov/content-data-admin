475899e766b754c8ef1bcf93ca500206ee02fa13
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-07-04 14:40:35 +0100

    Rubocop fixes

--- config/initializers/site_improve_api_client.rb
+++ config/initializers/site_improve_api_client.rb
@@ -1,6 +1,6 @@
 SiteImproveAPIClient.configure do |config|
-  config.username = ENV['SITE_IMPROVE_API_CLIENT_USERNAME']
-  config.password = ENV['SITE_IMPROVE_API_CLIENT_PASSWORD']
+  config.username = ENV["SITE_IMPROVE_API_CLIENT_USERNAME"]
+  config.password = ENV["SITE_IMPROVE_API_CLIENT_PASSWORD"]
 end
 
-Rails.application.config.site_improve_site_id = ENV['SITE_IMPROVE_SITE_ID']
+Rails.application.config.site_improve_site_id = ENV["SITE_IMPROVE_SITE_ID"]
