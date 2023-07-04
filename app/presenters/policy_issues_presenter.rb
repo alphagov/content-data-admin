4923e3132d9e47ec0594ce24e807ad15fc41278e
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-07-04 14:34:26 +0100

    Add basic links

--- app/presenters/policy_issues_presenter.rb
+++ app/presenters/policy_issues_presenter.rb
@@ -1,7 +1,12 @@
 class PolicyIssuesPresenter
-  attr_reader :issue_list
+  attr_reader :issue_list, :summary_info
 
-  def initialize(issue_list)
+  def initialize(issue_list, summary_info)
     @issue_list = issue_list
+    @summary_info = summary_info
+  end
+
+  def link
+    summary_info._siteimprove.policy.page_report.href
   end
 end
--- app/services/fetch_site_improve_summary.rb
+++ app/services/fetch_site_improve_summary.rb
@@ -10,6 +10,8 @@ class FetchSiteImproveSummary
   end
 
   def call
-    content_api.sites_site_id_content_pages_page_id_get(site_id, page_id_for_url(url: @url))
+    Rails.cache.fetch("site-improve-summary-for/#{@url}", expires_in: 1.hour) do
+      content_api.sites_site_id_content_pages_page_id_get(site_id, page_id_for_url(url: @url))
+    end
   end
 end
