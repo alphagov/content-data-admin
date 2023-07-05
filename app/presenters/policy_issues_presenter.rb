4fe7645e61a1c4f57db46b4aa0c61cc015e6de45
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-07-05 11:46:05 +0100

    Remove GDS ID from policy name

--- app/presenters/policy_issues_presenter.rb
+++ app/presenters/policy_issues_presenter.rb
@@ -10,7 +10,8 @@ class PolicyIssuesPresenter
     @policy_issues.map do |issue|
       {
         policy_category: issue.policy_category,
-        policy_name: issue.policy_name,
+        gds_policy_id: gds_policy_id(issue.policy_name),
+        policy_short_description: policy_short_description(issue.policy_name),
         policy_priority: issue.policy_priority,
         policy_description: FetchSiteImprovePolicies.new.find(issue.id).first.note,
         policy_direct_link: "#{link}#issue/#{issue.id}",
@@ -20,5 +21,13 @@ class PolicyIssuesPresenter
 
   def link
     summary_info._siteimprove.policy.page_report.href
+  end
+
+  def gds_policy_id(policy_name)
+    policy_name.split(" - ").first
+  end
+
+  def policy_short_description(policy_name)
+    policy_name.split(" - ").last
   end
 end
