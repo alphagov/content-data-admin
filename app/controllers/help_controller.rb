edb37050c1f3b7525b8f42f9bff34c86d5efc3c3
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-07-05 13:30:12 +0100

    Remove unused help controller

--- app/controllers/help_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class HelpController < ApplicationController
-  layout "application"
-
-  def show
-    @hkey = params[:hkey]
-  end
-end
--- spec/routing/help_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe "routes for Help" do
-  it "routes /help to Help" do
-    expect(get("/help")).to route_to("help#show")
-  end
-
-  it "does not route /help/<id>" do
-    expect(get("/help/1")).not_to be_routable
-  end
-end
