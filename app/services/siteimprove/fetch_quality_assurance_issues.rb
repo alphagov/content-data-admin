d46439122b2a061f038a124fe155c1c4fe179a2a
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-07-12 17:45:22 +0100

    Add service to get quality assurance issues
    Unlike policy issues, the quality assurance API has a differently
    structured API call for misspellings and broken links.
    As there is little commonality between the calls, they have been kept
    as separate methods.
    
    Co-authored-by: Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
    Co-authored-by: Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
    Co-authored-by: Max Harden <max.harden@digital.cabinet-office.gov.uk>

---
+++ app/services/siteimprove/fetch_quality_assurance_issues.rb
@@ -0,0 +1,32 @@
+module Siteimprove
+  class FetchQualityAssuranceIssues
+    include Siteimprove::ApiSupport
+
+    def quality_assurance_api
+      @quality_assurance_api ||= SiteimproveAPIClient::QualityAssuranceApi.new
+    end
+
+    def self.call(**args)
+      new(**args).call
+    end
+
+    def initialize(url:)
+      @url = url
+    end
+
+    def call
+      {
+        misspellings:,
+        broken_links:,
+      }
+    end
+
+    def misspellings
+      quality_assurance_api.sites_site_id_quality_assurance_spelling_pages_page_id_misspellings_get(site_id, page_id_for_url(url: @url)).items
+    end
+
+    def broken_links
+      quality_assurance_api.sites_site_id_quality_assurance_links_pages_with_broken_links_page_id_broken_links_get(site_id, page_id_for_url(url: @url)).items
+    end
+  end
+end
