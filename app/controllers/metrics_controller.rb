81921a5d3eb930613346234f34c473d3cfb7bc76
Pete Goddard <pete@artofsoftware.co.uk>
2024-01-08 15:26:07 +0000

    Extract method for siteimprove lookup

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,7 +1,6 @@
 class MetricsController < ApplicationController
   def show
     time_period = params[:date_range] || "past-30-days"
-    base_path = params[:base_path]
 
     curr_period = DateRange.new(time_period)
     prev_period = curr_period.previous
@@ -15,24 +14,32 @@ class MetricsController < ApplicationController
       curr_period,
     )
 
-    if current_user.view_siteimprove?
-      begin
-        @summary_info = Siteimprove::FetchSummary.call(url: "https://www.gov.uk/#{params[:base_path]}")
-
-        raw_policy_issues = Siteimprove::FetchPolicyIssues.call(url: "https://www.gov.uk/#{params[:base_path]}")
-        @policy_issues = Siteimprove::PolicyIssuesPresenter.new(raw_policy_issues, @summary_info)
-        raw_quality_assurance_issues = Siteimprove::FetchQualityAssuranceIssues.call(url: "https://www.gov.uk/#{params[:base_path]}")
-        @quality_assurance_issues = Siteimprove::QualityAssuranceIssuesPresenter.new(raw_quality_assurance_issues, @summary_info)
-        @has_accessibility_info = @policy_issues.any? || @quality_assurance_issues.any?
-      rescue Siteimprove::BaseError
-        @has_accessibility_info = false
-      end
-    else
-      @has_accessibility_info = false
-    end
+    setup_siteimprove
   end
 
   rescue_from GdsApi::HTTPNotFound do
     render "errors/404", status: :not_found
+  end
+
+private
+
+
+  def base_path
+    params[:base_path]
+  end
+
+  def setup_siteimprove
+    return unless current_user.view_siteimprove?
+
+    begin
+      @summary_info = Siteimprove::FetchSummary.call(url: "https://www.gov.uk/#{base_path}")
+      raw_policy_issues = Siteimprove::FetchPolicyIssues.call(url: "https://www.gov.uk/#{base_path}")
+      @policy_issues = Siteimprove::PolicyIssuesPresenter.new(raw_policy_issues, @summary_info)
+      raw_quality_assurance_issues = Siteimprove::FetchQualityAssuranceIssues.call(url: "https://www.gov.uk/#{base_path}")
+      @quality_assurance_issues = Siteimprove::QualityAssuranceIssuesPresenter.new(raw_quality_assurance_issues, @summary_info)
+      @has_accessibility_info = @policy_issues.any? || @quality_assurance_issues.any?
+    rescue Siteimprove::BaseError
+      @has_accessibility_info = false
+    end
   end
 end
