1371f196180a2aa2709b00a3a32f32b9092c12ac
Pete Goddard <pete@artofsoftware.co.uk>
2024-01-09 15:32:06 +0000

    Add a message when there is a 404 from email-alert-api

--- spec/features/single_content_item_email_subs_spec.rb
+++ spec/features/single_content_item_email_subs_spec.rb
@@ -20,16 +20,34 @@ RSpec.describe "/metrics/base/path email subscription details", type: :feature d
     before do
       GDS::SSO.test_user = build(:view_email_subs_user)
       stub_metrics_page(base_path: "base/path", time_period: :past_30_days)
-      json = { subscriber_list_count: 3, all_notify_count: 10 }.to_json
-      stub_get_subscriber_list_metrics(path: "/base/path", response: json)
     end
 
     context "when there are subscriptions" do
+      before do
+        json = { subscriber_list_count: 3, all_notify_count: 10 }.to_json
+        stub_get_subscriber_list_metrics(path: "/base/path", response: json)
+      end
+
       it "shows the Email subscriptions section" do
         visit "/metrics/base/path"
         expect(page).to have_content("Email subscriptions")
         expect(page).to have_content("Subscribers: 3")
         expect(page).to have_content("All notification subscribers: 10")
+        expect(page).not_to have_content("No subscription information found")
+      end
+    end
+
+    context "when there is a 404 from email-alert-api" do
+      before do
+        stub_get_subscriber_list_metrics_not_found(path: "/base/path")
+      end
+
+      it "shows the no subscription info section" do
+        visit "/metrics/base/path"
+        expect(page).to have_content("Email subscriptions")
+        expect(page).to have_content("No subscription information found")
+        expect(page).not_to have_content("Subscribers:")
+        expect(page).not_to have_content("All notification subscribers:")
       end
     end
   end
