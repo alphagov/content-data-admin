4438dd1606dc353b31455084e6df8c0a3d9f42c3
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-02-26 12:07:57 +0000

    Use LinkTitleResolver to improve broken urls table

--- spec/presenters/siteimprove/quality_assurance_issues_presenter_spec.rb
+++ spec/presenters/siteimprove/quality_assurance_issues_presenter_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Siteimprove::QualityAssuranceIssuesPresenter do
     siteimprove_has_summary
     siteimprove_has_policies
     Rails.application.config.siteimprove_site_id = 1
+    @link_title_resolver = double
+    allow(@link_title_resolver).to receive(:title_for_link).and_return("Link Text")
   end
 
   after do
@@ -25,7 +27,7 @@ RSpec.describe Siteimprove::QualityAssuranceIssuesPresenter do
       siteimprove_has_misspellings
       siteimprove_has_no_broken_links
 
-      presenter = described_class.new(quality_assurance_issues, summary_info)
+      presenter = described_class.new(quality_assurance_issues, summary_info, @link_title_resolver)
       expect(presenter.any?).to be true
     end
 
@@ -33,7 +35,7 @@ RSpec.describe Siteimprove::QualityAssuranceIssuesPresenter do
       siteimprove_has_no_misspellings
       siteimprove_has_broken_links
 
-      presenter = described_class.new(quality_assurance_issues, summary_info)
+      presenter = described_class.new(quality_assurance_issues, summary_info, @link_title_resolver)
       expect(presenter.any?).to be true
     end
 
@@ -41,7 +43,7 @@ RSpec.describe Siteimprove::QualityAssuranceIssuesPresenter do
       siteimprove_has_misspellings
       siteimprove_has_broken_links
 
-      presenter = described_class.new(quality_assurance_issues, summary_info)
+      presenter = described_class.new(quality_assurance_issues, summary_info, @link_title_resolver)
       expect(presenter.any?).to be true
     end
 
@@ -49,7 +51,7 @@ RSpec.describe Siteimprove::QualityAssuranceIssuesPresenter do
       siteimprove_has_no_misspellings
       siteimprove_has_no_broken_links
 
-      presenter = described_class.new(quality_assurance_issues, summary_info)
+      presenter = described_class.new(quality_assurance_issues, summary_info, @link_title_resolver)
       expect(presenter.any?).to be false
     end
   end
@@ -61,12 +63,12 @@ RSpec.describe Siteimprove::QualityAssuranceIssuesPresenter do
     end
 
     it "returns spelling suggestions as a string" do
-      misspellings = described_class.new(quality_assurance_issues, summary_info).misspellings
+      misspellings = described_class.new(quality_assurance_issues, summary_info, @link_title_resolver).misspellings
       expect(misspellings.first[:suggestions]).to eq("fish, phish")
     end
 
     it "gets the direct link to the misspelling in the page report" do
-      misspellings = described_class.new(quality_assurance_issues, summary_info).misspellings
+      misspellings = described_class.new(quality_assurance_issues, summary_info, @link_title_resolver).misspellings
       expected_link = "https://my2.siteimprove.com/Inspector/1054012/QualityAssurance/Page?impmd=0&PageId=2#/Issue/Misspellings/0"
 
       expect(misspellings.first[:quality_assurance_direct_link]).to eq(expected_link)
@@ -80,7 +82,7 @@ RSpec.describe Siteimprove::QualityAssuranceIssuesPresenter do
     end
 
     it "gets the direct link to the broken link in the page report" do
-      broken_links = described_class.new(quality_assurance_issues, summary_info).broken_links
+      broken_links = described_class.new(quality_assurance_issues, summary_info, @link_title_resolver).broken_links
       expected_link = "https://my2.siteimprove.com/Inspector/1054012/QualityAssurance/Page?impmd=0&PageId=2#/Issue/BrokenLinks/0"
 
       expect(broken_links.first[:quality_assurance_direct_link]).to eq(expected_link)
