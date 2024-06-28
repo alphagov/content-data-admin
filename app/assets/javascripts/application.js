aace1c642ea8ed863faa19b99441fc9b88aef1ca
Murilo Dal Ri <murilodalri@gmail.com>
2024-06-28 10:27:47 +0100

    Upgrade to govuk frontend 5.1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
 //= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/all_components
+//= require govuk_publishing_components/components/details
+//= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/modal-dialogue
 //= require_tree ./components
 //= require_tree ./core
