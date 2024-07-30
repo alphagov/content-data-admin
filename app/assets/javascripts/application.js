f7f7a0d742523b74b911ebb02c8af0ee9f60bad9
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-07-30 17:37:54 +0100

    Fix breaking changes
    - details.js no longer exists

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 //= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/modal-dialogue
 //= require_tree ./components
