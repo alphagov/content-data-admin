77c7378b28840bfe5620163cf2637ab2ab4ae696
Ed Davey <edward.antoniodavey@dsit.gov.uk>
2026-09-14 12:23:10 +0100

    Remove use of defunct 'layout-header'
    See https://github.com/alphagov/govuk_publishing_components/pull/5507

--- app/assets/javascripts/es6-components.js
+++ app/assets/javascripts/es6-components.js
@@ -9,5 +9,4 @@
 
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/radio
-//= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/skip-link
